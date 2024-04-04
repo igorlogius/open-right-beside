cb00ed2b7a6802713196242b5b4343c762f911f2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2024-04-04 17:52:13 +0200

    chore: cleanup

--- background.js
+++ background.js
@@ -4,23 +4,22 @@
 let prevTabId = null;
 let actiTabId = null;
 async function onActivated(activeInfo) {
-    prevTabId = activeInfo.previousTabId;
-    actiTabId = activeInfo.tabId;
+  prevTabId = activeInfo.previousTabId;
+  actiTabId = activeInfo.tabId;
 }
 
 async function onCreated(newTab) {
-    let activeTab = null;
-    if(newTab.id === actiTabId){
-        activeTab = await browser.tabs.get(prevTabId);
-    }else{
-        activeTab = await browser.tabs.get(actiTabId);
-    }
-    if(newTab.windowId === activeTab.windowId){
-        browser.tabs.move(newTab.id, { index: activeTab.index + 1});
-        prevTabId = newTab.id;
-    }
+  let activeTab = null;
+  if (newTab.id === actiTabId) {
+    activeTab = await browser.tabs.get(prevTabId);
+  } else {
+    activeTab = await browser.tabs.get(actiTabId);
+  }
+  if (newTab.windowId === activeTab.windowId) {
+    browser.tabs.move(newTab.id, { index: activeTab.index + 1 });
+    prevTabId = newTab.id;
+  }
 }
 
 browser.tabs.onActivated.addListener(onActivated);
 browser.tabs.onCreated.addListener(onCreated);
-
